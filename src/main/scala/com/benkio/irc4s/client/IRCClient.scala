61e00f5ab195ae76abb847e20066a0f5890f32a4
Enrico Benini <benkio89@gmail.com>
2022-02-12 00:16:19 +0000

    `SendPrivateMessage` + `JoinChannelCommand` commands. Implemented some IRCClient functions. Add port

--- src/main/scala/com/benkio/irc4s/client/IRCClient.scala
+++ src/main/scala/com/benkio/irc4s/client/IRCClient.scala
@@ -2,7 +2,11 @@ package com.benkio.irc4s.client
 
 import com.benkio.irc4s.protocol.IRCCommand
 import com.benkio.irc4s.protocol.QuitCommand
-import com.comcast.ip4s._
+import com.benkio.irc4s.protocol.JoinChannelCommand
+import com.benkio.irc4s.protocol.SendPrivateMessage
+import com.comcast.ip4s.Host
+import com.comcast.ip4s.Port
+import com.comcast.ip4s.SocketAddress
 import fs2.Stream
 import fs2.io.net.Network
 import fs2.io.net.Socket
@@ -13,22 +17,31 @@ import cats.implicits._
 
 trait IRCClient[F[_]: Async: Logger]:
   def joinChannel(channel: String): F[Unit]
-  def sendCommand(command: IRCCommand, message: String): F[Unit]
+  def sendCommand(command: IRCCommand, message: Option[String]): F[Unit]
   def sendMessageTo(recepient: String, message: String): F[Unit]
   def readMessages(): Stream[F, String]
 
 object IRCClient:
+
+  final case class InvalidPortException(port: Int) extends Throwable(s"[IRCClient] Invalid Port: $port")
+
   def connect[F[_]: Async: Logger](server: String, port: Int = 6667): Resource[F, IRCClient[F]] = for {
-    socket <- Network[F].client(SocketAddress(Host.fromString(server).get, port"80"))
+    portValidated <- Resource.eval(
+      Port.fromInt(port).fold(Async[F].raiseError(InvalidPortException(port)))(Async[F].pure(_))
+    )
+    socket <- Network[F].client(SocketAddress(Host.fromString(server).get, portValidated))
     _      <- Resource.eval(Logger[F].debug(s"Socket created at $server : $port"))
     ircClient <- Resource.make(Async[F].pure(new IRCClientImpl[F](socket)))(client =>
-      Logger[F].debug(s"quitting from $server : $port") >> client.sendCommand(QuitCommand, "Goodbye")
+      Logger[F].debug(s"quitting from $server : $port") >> client.sendCommand(QuitCommand, Some("Goodbye"))
     )
-
   } yield ircClient
 
   private class IRCClientImpl[F[_]: Async: Logger](socket: Socket[F]) extends IRCClient[F]:
-    def joinChannel(channel: String): F[Unit]                      = ???
-    def sendCommand(command: IRCCommand, message: String): F[Unit] = ???
-    def sendMessageTo(recepient: String, message: String): F[Unit] = ???
-    def readMessages(): Stream[F, String]                          = ???
+    def joinChannel(channel: String): F[Unit] = for {
+      joinChannelCommand <- Async[F].fromEither(JoinChannelCommand(channel))
+      _                  <- sendCommand(joinChannelCommand, None)
+    } yield ()
+    def sendCommand(command: IRCCommand, message: Option[String]): F[Unit] = ???
+    def sendMessageTo(recepient: String, message: String): F[Unit] =
+      sendCommand(SendPrivateMessage(recepient), Some(message))
+    def readMessages(): Stream[F, String] = ???
--- src/main/scala/com/benkio/irc4s/protocol/IRCCommand.scala
+++ src/main/scala/com/benkio/irc4s/protocol/IRCCommand.scala
@@ -1,4 +1,17 @@
 package com.benkio.irc4s.protocol
 
 sealed trait IRCCommand
-case object QuitCommand extends IRCCommand
+case object QuitCommand                                extends IRCCommand
+final case class SendPrivateMessage(recipient: String) extends IRCCommand
+final case class JoinChannelCommand(channel: String)   extends IRCCommand
+object JoinChannelCommand {
+
+  final case class InvalidChannelName(channel: String) extends Throwable(s"Invalid Channel Name: $channel")
+
+  def apply(channel: String): Either[Throwable, JoinChannelCommand] =
+    Either.cond(
+      List("#", "&", "!", "+").exists(prefix => channel.startsWith(prefix)),
+      new JoinChannelCommand(channel),
+      InvalidChannelName(channel)
+    )
+}
